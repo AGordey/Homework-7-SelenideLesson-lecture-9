5dac83eddebc8151d745c6c283384638435ef406
AGordey <gordey.andrej@gmail.com>
2022-04-24 23:11:18 +0300

    Third part task done

---
+++ src/test/java/guru/qa/AnswerForThirdTaskOnHomework.java
@@ -0,0 +1,18 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AnswerForThirdTaskOnHomework {
+
+    @Test
+    void DragDropTest() {
+
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
+
